96607deffdfe5a6285ce02e288580778c47e213e
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-02-24 07:55:39 +0530

    Initial commit

---
+++ contracts/coffeeaccesscontrol/RetailerRole.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.24;
+
+// Import the library 'Roles'
+import "./Roles.sol";
+
+// Define a contract 'RetailerRole' to manage this role - add, remove, check
+contract RetailerRole {
+    // Define 2 events, one for Adding, and other for Removing
+    event AddRetailer(address retailerAddress);
+    event RemoveRetailer(address retailerAddress);
+
+    // Define a struct 'retailers' by inheriting from 'Roles' library, struct Role
+    using Roles for Roles.Role;
+    Roles.Role private retailers;
+
+    // In the constructor make the address that deploys this contract the 1st retailer
+    constructor() public {
+        addRetailer(msg.sender);
+    }
+
+    // Define a modifier that checks to see if msg.sender has the appropriate role
+    modifier onlyRetailer() {
+        require(isRetailer(msg.sender));
+        _;
+    }
+
+    // Define a function 'isRetailer' to check this role
+    function isRetailer(address account) public view returns (bool) {
+        return retailers.has(account);
+    }
+
+    // Define a function 'addRetailer' that adds this role
+    function addRetailer(address account) public onlyRetailer {
+        _addRetailer(account);
+    }
+
+    // Define a function 'renounceRetailer' to renounce this role
+    function renounceRetailer() public {
+        _removeRetailer(msg.sender);
+    }
+
+    // Define an internal function '_addRetailer' to add this role, called by 'addRetailer'
+    function _addRetailer(address account) internal {
+        retailers.add(account);
+        emit AddRetailer(account);
+    }
+
+    // Define an internal function '_removeRetailer' to remove this role, called by 'removeRetailer'
+    function _removeRetailer(address account) internal {
+        retailers.remove(account);
+        emit RemoveRetailer(account);
+    }
+}
---
+++ contracts/coffeeaccesscontrol/Roles.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @title Roles
+ * @dev Library for managing addresses assigned to a Role.
+ */
+library Roles {
+    struct Role {
+        mapping(address => bool) bearer;
+    }
+
+    modifier isNotZeroAddress(address account) {
+        require(account != address(0));
+        _;
+    }
+
+    /**
+     * @dev give an account access to this role
+     */
+    function add(Role storage role, address account)
+        internal
+        isNotZeroAddress(account)
+    {
+        require(!has(role, account));
+        role.bearer[account] = true;
+    }
+
+    /**
+     * @dev remove an account's access to this role
+     */
+    function remove(Role storage role, address account)
+        internal
+        isNotZeroAddress(account)
+    {
+        require(has(role, account));
+        role.bearer[account] = false;
+    }
+
+    /**
+     * @dev check if an account has this role
+     * @return bool
+     */
+    function has(Role storage role, address account)
+        internal
+        view
+        isNotZeroAddress(account)
+        returns (bool)
+    {
+        return role.bearer[account];
+    }
+}
